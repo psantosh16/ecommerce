1e73d072c79a6e22916db3778c2a43c9186e3ed5
Santosh Phadtare <santoshphadtare1610@gmail.com>
2024-06-16 17:29:49 +0530

    Update contentful.ts

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -2,7 +2,7 @@ import { createClient } from "contentful";
 
 const client = createClient({
   space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID as string,
-  accessToken: process.NEXT_PUBLIC_env.CONTENTFUL_ACCESS_TOKEN as string,
+  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN as string,
 });
 
 export default client;
